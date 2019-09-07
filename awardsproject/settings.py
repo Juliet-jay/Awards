0b5b88137e6401a176f83d3588f45f5c6c72639c
kathure <juliekmuthee@gmail.com>
2019-09-08 01:28:41 +0300

    create templates folder and files

--- awardsproject/settings.py
+++ awardsproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'awardsapp',
+    'awardsapp',
     # 'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
