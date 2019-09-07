e714f61b750daf9dbfb00d57d7d082c60b6e58ab
kathure <juliekmuthee@gmail.com>
2019-09-08 01:58:14 +0300

    add database list

--- awardsproject/settings.py
+++ awardsproject/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'awardsproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awards',
+        'USER': 'kathure',
+    'PASSWORD':'tired101',
     }
 }
 
@@ -119,7 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
