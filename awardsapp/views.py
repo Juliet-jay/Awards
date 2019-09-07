95d3634e0d67b5b1080ad360f7ff4c1d207c72b9
kathure <juliekmuthee@gmail.com>
2019-09-08 01:39:58 +0300

    add function project

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -46,43 +46,43 @@ def home_projects (request):
 
     return render(request, 'index.html', {'projects':projects, 'letterForm':form})
 
-# def project(request, id):
+def project(request, id):
 
-#     try:
-#         project = Project.objects.get(pk = id)
+    try:
+        project = Project.objects.get(pk = id)
 
-#     except DoesNotExist:
-#         raise Http404()
+    except DoesNotExist:
+        raise Http404()
 
-#     current_user = request.user
-#     comments = Review.get_comment(Review, id)
-#     latest_review_list=Review.objects.all()
+    current_user = request.user
+    comments = Review.get_comment(Review, id)
+    latest_review_list=Review.objects.all()
 
-#     if request.method == 'POST':
-#         form = ReviewForm(request.POST)
-#         if form.is_valid():
-#             design_rating = form.cleaned_data['design_rating']
-#             content_rating = form.cleaned_data['content_rating']
-#             usability_rating = form.cleaned_data['usability_rating']
-#             comment = form.cleaned_data['comment']
-#             review = Review()
-#             review.project = project
-#             review.user = current_user
-#             review.comment = comment
-#             review.design_rating = design_rating
-#             review.content_rating = content_rating
-#             review.usability_rating = usability_rating
-#             review.save()
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            design_rating = form.cleaned_data['design_rating']
+            content_rating = form.cleaned_data['content_rating']
+            usability_rating = form.cleaned_data['usability_rating']
+            comment = form.cleaned_data['comment']
+            review = Review()
+            review.project = project
+            review.user = current_user
+            review.comment = comment
+            review.design_rating = design_rating
+            review.content_rating = content_rating
+            review.usability_rating = usability_rating
+            review.save()
 
-#     else:
-#         form = ReviewForm()
+    else:
+        form = ReviewForm()
 
-#         # return HttpResponseRedirect(reverse('image', args=(image.id,)))
+        # return HttpResponseRedirect(reverse('image', args=(image.id,)))
 
-#     return render(request, 'image.html', {"project": project,
-#                                           'form':form,
-#                                           'comments':comments,
-#                                           'latest_review_list':latest_review_list})
+    return render(request, 'image.html', {"project": project,
+                                          'form':form,
+                                          'comments':comments,
+                                          'latest_review_list':latest_review_list})
 
 # @login_required(login_url='/accounts/login/')
 # def new_image(request):
