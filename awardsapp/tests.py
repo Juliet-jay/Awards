b1f3c3ef598e7cb83606233beb2592dbb2b6ae9a
kathure <juliekmuthee@gmail.com>
2019-09-08 02:05:35 +0300

    import location,tags,reviews

--- awardsapp/tests.py
+++ awardsapp/tests.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from .models import Location, tags, Image, Review, User, Profile
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 # Create your tests here.
