57dc124389930f02451f0acd9b4b27849c6492e1
kathure <juliekmuthee@gmail.com>
2019-09-08 02:08:06 +0300

    add class tagtestclass

--- awardsapp/tests.py
+++ awardsapp/tests.py
@@ -6,3 +6,10 @@ from .models import Location, tags, Image, Review, User, Profile
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 # Create your tests here.
+class tagsTestClass(TestCase):
+    
+    # Set up method the test for location and instantiating the location object
+
+    def setUp(self):
+        self.test_tags = tags(name='funny')
+        self.test_tags.save()
