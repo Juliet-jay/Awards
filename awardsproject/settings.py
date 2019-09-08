5660dfa1cc5b7351186da73c3611c741e65b1eb9
kathure <juliekmuthee@gmail.com>
2019-09-08 03:32:34 +0300

    add rest framework on installed apps

--- awardsproject/settings.py
+++ awardsproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'awardsapp',
     'bootstrap3',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
